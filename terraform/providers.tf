b4491174b78fa9b30d6427dfda421728c2670fe7
Admin <sonu.parit1@gmail.com>
2026-03-12 13:52:40 +0530

    changed aws version

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.95.0"
+      version = ">= 5.95.0"
     }
     helm = {
       source  = "hashicorp/helm"
