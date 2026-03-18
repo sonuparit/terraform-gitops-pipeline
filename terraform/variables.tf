2b58c5bc2c229e8887b4013a330254d790ba7b5b
Sonu Parit <sonu.parit1@gmail.com>
2026-03-18 18:06:28 +0530

    Added final touches

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@
 variable "aws_region" {
   description = "AWS region where resources will be created"
   type        = string
-  default     = "us-west-2"
+  default     = "ap-south-1"
 }
 
 variable "cluster_name" {
@@ -23,7 +23,7 @@ variable "environment" {
 variable "kubernetes_version" {
   description = "Kubernetes version for EKS cluster"
   type        = string
-  default     = "1.33"
+  default     = "1.34"
 }
 
 variable "vpc_cidr" {
